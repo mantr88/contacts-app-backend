7ec6f031d10b3280c787aa1f91dbd83bd2963fdc
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-11-29 23:11:06 +0200

    fix: authenticate midleware

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,26 +1,36 @@
 const jwt = require("jsonwebtoken");
 
-const SECRET_KEY = process.env.SECRET_KEY;
+const JWT_ACCESS_SECRET_KEY = process.env.JWT_ACCESS_SECRET_KEY;
 
 const { User } = require("../models/users/index");
 
 const authenticate = async (req, res, next) => {
-  const { authorization = "" } = req.headers;
-  const [bearer, token] = authorization.split(" ");
-
-  if (bearer !== "Bearer") {
-    return res.status(401).json("Not authorized");
-  }
   try {
-    const { id } = jwt.verify(token, SECRET_KEY);
+    console.log("0");
+    const { authorization = "" } = req.headers;
+    console.log(typeof authorization);
+    const [bearer, accessToken] = authorization.split(" ");
+    console.log(bearer);
+    console.log(accessToken);
+    if (bearer !== "Bearer") {
+      return res.status(401).json("Not authorized");
+    }
+    console.log("1");
+    if (!accessToken) {
+      return res.status(401).json("Not authorized");
+    }
+    console.log("2");
+
+    const { id } = jwt.verify(accessToken, JWT_ACCESS_SECRET_KEY);
     const user = await User.findById(id);
-    if (!user.token) {
+    if (!user) {
+      console.log("3");
       return res.status(401).json("Not authorized");
     }
     req.user = user;
     next();
   } catch (error) {
-    return res.status(401).json("Not authorized");
+    return res.status(401).json("4Not authorized");
   }
 };
 
